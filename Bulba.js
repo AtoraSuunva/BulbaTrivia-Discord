6bfdfc829c0f7a11cef9933e534d2a59dc4c42f3
Diego Plaza <AtlasTheBot@users.noreply.github.com>
2016-07-07 21:11:40 -0400

    everything really
    If all goes well, this should be the only necessary commit

---
+++ Bulba.js
@@ -0,0 +1,174 @@
+/*jshint esversion: 6 */
+/*jshint evil: true*/
+//MUAHAHAHAHA I'M EVIL
+
+//SETUP
+
+const Discord = require('discord.js'); //Handles the API
+const tumblr = require('tumblr'); //tumblr woo
+const Auth = require('./auth.json'); //Auth details
+var entities = require("entities"); //fug enities
+
+const version = '1.0.0';
+
+var totalPosts = 0; //For random quotes, you'll see why later
+var currentOffset = 0;
+
+var oauth = {
+  consumer_key: Auth.tumblr.token
+};
+
+var bulba = new Discord.Client({
+  "autoReconnect": true
+});
+
+var blog = new tumblr.Blog('thanksbulbapedia.tumblr.com', oauth);
+
+const inviteLink = "https://discordapp.com/oauth2/authorize?client_id=200723686205030400&scope=bot&permissions=0";
+
+bulba.on('ready', function() {
+  bulba.setPlayingGame('+bulba | +bulba help');
+
+  blog.quote({limit: 1}, function(error, response) {
+    if (error) {
+      throw new Error(error);
+    }
+
+    totalPosts = response.total_posts;
+  });
+
+  console.log('Everything is ready!');
+});
+
+//ACTUAL BOT
+bulba.on('message', function(message) { //switch is for the weak
+  if (message.author.equals(bulba.user) || message.author.bot) return; //Don't reply to itself or bots
+
+  message.content = cleanMessage(message); //Clean stuff between `` so it doesn't bother reading code
+
+  if(message.content.indexOf('+bulba help') === 0) {
+    bulba.sendMessage(message.channel, '```xl' + '\n' +
+      '+bulba        => Trivia from BulbaPedia.' + '\n' +
+      '+bulba info   => Info about the bot.' + '\n' +
+      '+bulba help   => Help menu (you know, this one).' + '\n' +
+      '+bulba invite => Invite this useless bot.' + '\n' +
+      '```');
+    return;
+  }
+
+  if(message.content.indexOf('+bulba info') === 0) {
+    bulba.sendMessage(message.channel,
+      'BulbaTrivia V' + version + ' by AtlasTheBot (@Atlas)' + '\n' +
+      'Source: https://github.com/AtlasTheBot/BulbaTrivia-Discord' + '\n' +
+      'Official Chat: https://discord.gg/0w6AYrrMIUfO71oV' + '\n' +
+      'Source of quotes: https://thanksbulbapedia.tumblr.com' + '\n' +
+      'Made with tears and love');
+
+    return;
+  }
+
+  if(message.content.indexOf('+bulba invite') === 0) {
+    bulba.sendMessage(message.channel,
+      'Invite Link: ' + inviteLink + '\n' +
+      'but why?');
+
+    return;
+  }
+
+  if(message.content.indexOf('+bulba') === 0) {
+    currentOffset = randInt(totalPosts - 1); //From 0 to totalPosts, to get a random quote
+    //-1 because after an offset of totalPosts there's nothing
+
+    //Quick tutorial on how I get random quotes
+    //1. Get the number of posts
+    //2. Get a random number from 0 to totalPosts
+    //3. Use that as a offset
+    //it's horrible but it works sooo
+
+    blog.quote({limit: 1, offset: currentOffset}, function(error, response) {
+      if (error) {
+        bulba.sendMessage(message.channel, 'Sorry! Something went wrong! Tell Atlas `that\' it, I quit`.');
+        throw new Error(error);
+      }
+
+      if (response.posts[0] !== undefined) {
+        totalPosts = response.total_posts; //update because why not
+
+        bulba.sendMessage(message.channel,
+          '*"' + entities.decodeHTML(response.posts[0].text) + '"*' + '\n' +
+          '—__' + response.posts[0].source.match(/<a.*?>(.*?)<\/a>/)[1] + '__');
+
+        console.log('Showed ' + response.posts[0].short_url + ' to ' + message.author.name + '#' + message.author.discriminator + ' (' + message.author.id + ')');
+      } else {
+        bulba.sendMessage(message.channel, 'Sorry! Something went wrong! Tell Atlas `posts undefined`.');
+      }
+    });
+
+    return; //stop
+  }
+
+  if(message.content.indexOf('++bulba eval') === 0) {
+
+    if (message.author.id !== "74768773940256768") { //ain't nobody else runnin' eval on my watch
+      bulba.sendMessage(message.channel, 'Nice try, but no.');
+      return;
+    }
+
+    var content = message.content.replace('++bulba eval', '');
+
+    console.log('-------------------------EVAL STUFF');
+
+    try {
+      var result = eval(content);
+      console.log(result);
+      bulba.sendMessage(message.channel, '`' + result + '`');
+    } catch (err) {
+      console.log(err);
+      bulba.sendMessage(message.channel, '`' + err + '`');
+    }
+  }
+});
+
+//FUNCTIONS
+
+const thingsToClean = [/`{1,}\n?(.*\n?)*`{1,}/]; //In case I want to expand the black list later
+//HORRIBLE CONFUSING REGEX GO!
+
+function cleanMessage(message) { //wosh your code
+  var cleanContent = message.content; //wow well done look at the effort
+
+  for (i = 0; i <= thingsToClean.length; i++) { //JSHint screams if you declare i (var i)
+      cleanContent = cleanContent.replace(thingsToClean[i], '');
+  }
+
+  return cleanContent;
+}
+
+function randInt(max) { //1-max, for the offset
+    return Math.floor(Math.random() * (max));
+}
+
+//AUTH STUFF
+
+if (Auth.discord.token !== '') {
+  console.log('Logged in with token!');
+  bulba.loginWithToken(Auth.discord.token);
+
+} else if (Auth.discord.email !== '' && Auth.discord.password !== '') {
+  bulba.login(Auth.discord.email, Auth.discord.password, function (error, token) {
+    console.log('Logged in with email + pass!');
+    Auth.discord.token = token;
+
+    fs.writeFile('./auth.json', JSON.stringify(Auth, null, 4), function(err) {
+      if(err) {
+        console.log(err + '\n===\nError while saving token');
+      } else {
+        console.log('Token saved');
+      }
+    });
+
+  });
+} else {
+  console.log('No authentication details found!');
+  process.exit(1);
+}
---
+++ README.md
@@ -0,0 +1,70 @@
+# BulbaTrivia
+
+When you need some really fun (useless) trivia for Pokemon.
+
+# Preview
+
+Test it on Discord [here](https://discord.gg/0w6AYrrMIUfO71oV)!
+
+# How to use
+
+## Connect to Discord
+
+Literally click 4 times.
+
+0. Click [here](https://discordapp.com/oauth2/authorize?client_id=200723686205030400&scope=bot&permissions=0).
+1. Select a server.
+2. Authorize.
+3. gg no re
+
+## Manual Setup
+
+Just contact me (or read the setup for [RobotOtter](https://github.com/AtlasTheBot/RobotOtter-Discord#manual-setup)).
+
+You need to install the dependencies however (and tokens for Discord/Tumblr) so please don't do this to yourself.
+
+# Commands
+
+## +bulba
+Gets some trivia
+
+*Example: +bulba =>
+
+    *"Some claim that if one were to play backwards the Pokérap, “Gotta Catch ‘em All!” can be heard as “I love you, Satan”*."
+    —http://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9mon_controversy#Satanism
+
+## +bulba info
+Info about the bot
+
+
+*Example: +bulba info =>*
+
+    BulbaTrivia V1 by AtlasTheBot (@Atlas)
+    Source: https://github.com/AtlasTheBot/BulbaTrivia-Discord
+    Official Chat: https://discord.gg/0w6AYrrMIUfO71oV
+    Made with tears and love
+
+## +bulba invite
+Invite link.
+
+*Example: +bulba invite =>*
+
+    Invite Link: https://discordapp.com/oauth2/authorize?client_id=200723686205030400&scope=bot&permissions=0
+
+## +bulba help
+Instant, in-chat help for when you forget how to type `+bulba`.
+
+*Example: +bulba help =>*
+
+    +bulba        => Trivia from BulbaPedia.
+    +bulba info   => Info about the bot.
+    +bulba help   => Help menu (you know, this one).
+    +bulba invite => Invite this useless bot.
+
+
+## License
+Whoever uses this project (The USER) must agree to never hit Atlas (The AUTHOR) with a cactus. Or insert said cactus into any orifices. The AUTHOR may request the USER at any time to "Never talk to me or my bot ever again." or to "Drop and give me 20". The USER must also admit to the AUTHOR that otters are "pretty neat animals" when asked about them. Any and all violation of these clauses will allow the AUTHOR to take one (1) soul from the offending USER. These clauses extend for as long as the bot is in use, so not even through death can the USER excape the AUTHOR.
+
+Also I don't know what any of the licenses mean or be bothered to keep track of who uses it.
+
+Although if you do use this (God knows why), then I'd love to hear about it!
