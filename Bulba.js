22b3e845f9b26a174437d401c67c421ff4a2c2f3
Diego Plaza <AtlasTheBot@users.noreply.github.com>
2016-07-09 18:12:42 -0400

    em dash pls

--- Bulba.js
+++ Bulba.js
@@ -9,7 +9,7 @@ const tumblr = require('tumblr'); //tumblr woo
 const Auth = require('./auth.json'); //Auth details
 var entities = require("entities"); //fug enities
 
-const version = '1.0.1';
+const version = '1.0.2';
 
 var totalPosts = 0; //For random quotes, you'll see why later
 var currentOffset = 0;
@@ -93,11 +93,11 @@ bulba.on('message', function(message) { //switch is for the weak
 
       if (response.posts[0] !== undefined) {
         totalPosts = response.total_posts; //update because why not
-		
+
 		if (response.posts[0].source.match(/<a.*?>(.*?)<\/a>/)[1] !== undefined) { //quick and dirty fix ;)
 			bulba.sendMessage(message.channel,
 			  '*"' + entities.decodeHTML(response.posts[0].text) + '"*' + '\n' +
-			  response.posts[0].source.match(/<a.*?>(.*?)<\/a>/)[1]);
+			  '—' + response.posts[0].source.match(/<a.*?>(.*?)<\/a>/)[1]);
 		} else {
 			ulba.sendMessage(message.channel,
 			  '*"' + entities.decodeHTML(response.posts[0].text) + '"*' + '\n' +
