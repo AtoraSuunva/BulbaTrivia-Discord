fab089a91fc267280589a9045c9cdf53a1e8ebb6
AtlasTheBot <AtlasTheBot@users.noreply.github.com>
2017-12-23 00:32:34 -0500

    Update to module system

---
+++ modules/misc/invite.js
@@ -0,0 +1,25 @@
+//Invite the butt
+
+module.exports.config = {
+  name: 'invite',
+  invokers: ['invite'],
+  help: 'Get an invite for the bot/the help server!',
+  expandedHelp: 'Use `bulba invite` to invite the bot, `bulba invite server` for an invite to the help server (DMed to you) so you can yell at me.'
+}
+
+module.exports.events = {}
+module.exports.events.message = (bot, message) => {
+  let settingsId = (message.guild !== null) ? message.guild.id : message.channel.id
+  let settings = bot.modules.settings.get(settingsId)
+  let args = bot.modules.shlex(message.content)
+
+  if (args[1] === 'server' || args[1] === 'guild') {
+    message.author.send('https://discord.gg/0w6AYrrMIUfO71oV')
+    return message.channel.send('Sent! Check your DMs')
+  }
+
+  bot.generateInvite(['EMBED_LINKS'])
+    .then(link => {
+      message.channel.send(`Invite me! ${link}\nUse \`bulba invite server\` for the help server!`);
+    })
+}
