e50b823ff728dcb477fb439a98343d6babb06b18
Atora <15206032+AtoraSuunva@users.noreply.github.com>
2022-01-05 02:30:39 -0500

    fix(trivia): fetch tumblrs API directly instead of using a package

--- src/modules/bulba/bulbatrivia.js
+++ src/modules/bulba/bulbatrivia.js
@@ -1,61 +1,63 @@
+// @ts-check
 // Yay Trivia
-const config = (module.exports.config = {
+module.exports.config = {
   name: 'trivia',
   invokers: ['trivia'],
   help: 'Get some fun "trivia".',
   expandedHelp: 'Just run it fam',
-})
-
-const Discord = require('discord.js')
-const tumblr = require('tumblr')
-const entities = require('entities')
-const auth = require('./auth.json')
-
-const blog = new tumblr.Blog('thanksbulbapedia.tumblr.com', {
-  consumer_key: auth.tumblr,
-})
-
-let totalPosts = 0
-
-blog.quote({ limit: 1 }, (e, r) => {
-  if (e) throw e
-
-  totalPosts = r.total_posts
-})
-
-module.exports.events = {}
-module.exports.events.message = (bot, message) => {
-  const [cmd] = bot.sleet.shlex(message)
-
-  if (cmd && !config.invokers.includes(cmd.toLowerCase())) return
-
-  blog.quote({ limit: 1, offset: randOffset(totalPosts - 1) }, (e, r) => {
-    if (e) {
-      message.channel.send('Something went wrong while getting the quote...')
-      bot.logger.error(e)
-    }
-
-    if (r.posts[0] == undefined) return
-
-    totalPosts = r.total_posts
-
-    let text = clean(r.posts[0].text).replace(/<br\/?>/g, '')
-    let source = r.posts[0].source
-
-    if (
-      r.posts[0].source &&
-      r.posts[0].source.match(/<a.*?>(.*?)<\/a>/)[1] !== undefined
-    )
-      source = source.match(/<a.*?>(.*?)<\/a>/)[1]
-
-    message.channel.send(`*"${text}"*\n` + `—${source}`)
-  })
 }
 
-function clean(str) {
-  return Discord.Util.escapeMarkdown(entities.decodeHTML(str))
+const fetch = require('node-fetch')
+
+const KEY = process.env.TUMBLR_CONSUMER_KEY
+const BLOG_NAME = 'thanksbulbapedia'
+const API_BASE = 'https://api.tumblr.com/v2/'
+const QUOTE_BASE = `${API_BASE}blog/${BLOG_NAME}/posts/quote`
+
+// fetch quotes from a tumblr blog
+async function fetchQuotes(options) {
+  const searchParams = new URLSearchParams({
+    api_key: KEY,
+    filter: 'text',
+    limit: options.limit || 1,
+  })
+
+  if (options.offset !== undefined) {
+    searchParams.set('offset', options.offset)
+  }
+
+  const url = `${QUOTE_BASE}?${searchParams}`
+  const res = await fetch(url).then(res => res.json())
+
+  totalPosts = res.response.total_posts
+  return res.response.posts
+}
+
+let totalPosts = 1
+
+// Fetch one post to initialize totalPosts
+fetchQuotes({ limit: 1 })
+
+module.exports.events = {}
+module.exports.events.message = async (bot, message) => {
+  const offset = randOffset(totalPosts - 1)
+  const posts = await fetchQuotes({ limit: 1, offset })
+  const post = posts[0]
+
+  if (!post) {
+    return message.channel.send('I failed to fetch a quote.')
+  }
+
+  const text = post.text.replace(/\n/g, '*\n> *')
+  const source = post.source
+  const url = post.post_url
+
+  message.channel.send(`> *"${text}"*\n> —<${source}>\n> From <${url}>`)
+}
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function randOffset(max) {
-  return Math.floor(Math.random() * max)
+  return randomInt(0, max)
 }
